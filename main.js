faaaa83aa6b270dc88d2fa451c6339b0db0fb07b
omarsalah25 <aliomerasdf@gmail.com>
2023-03-24 05:17:30 +0200

    innit

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import "./style.css";
 import Experience from "./Experience/Experience.js";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
